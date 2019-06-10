6ccd9d6d96d97eb7a8bca998ce08022ca04c9654
shinmura0 <34497776+shinmura0@users.noreply.github.com>
2019-06-10 13:01:27 +0900

    update

--- exp/texture.py
+++ exp/texture.py
@@ -131,9 +131,5 @@ with tf.Session() as sess:
             #path = osp.join('results/texture/'+args.data, args.kern, 'epoch_{}.png'.format(epoch))
             #makedirs(path)
             #mpimg.imsave(path, res, cmap=plt.get_cmap('gray'))
-            res = np.array(res)
-            plt.imshow(res,'gray')
+            plt.imshow(res)
             plt.show()
-            
-    plt.imshow(res,'gray')
-    plt.show()
